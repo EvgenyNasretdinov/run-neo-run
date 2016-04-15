8d8cfb898145f299d9704c11cd8a6ff2f45a3ed7
Evgeny Nasretdinov <evgeny.nasretdinov@gmail.com>
2016-04-15 04:46:16 +0200

    map-test for navigation view

--- js/gamepreload.js
+++ js/gamepreload.js
@@ -2,11 +2,16 @@ var gamePreload = function(game) {}
 
             gamePreload.prototype = {
                 preload: function(){
-                    this.game.load.image("gameoverImage","images/gameover-button.png");
-                    this.game.load.image("addscoreImage","images/addscore-button.png");
+                    this.game.load.image("gameoverImage", "images/gameover-button.png");
+                    this.game.load.image("addscoreImage", "images/addscore-button.png");
+                    this.game.load.tilemap("map", "tilemaps/test.json", null, Phaser.Tilemap.TILED_JSON);
+                    this.game.load.image("tiles", "images/tiles.png");
                 },
                 create: function(){
-
+                    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+                    this.scale.pageAlignHorizontally = true;
+                    this.scale.pageAlignVertically = true;
+                    this.game.physics.startSystem(Phaser.Physics.ARCADE);
                     this.game.state.start("GameStart");
                 }
             }
--- js/gamestart.js
+++ js/gamestart.js
@@ -1,17 +1,27 @@
 var gameStart = function (game) {
                 score = 0;
                 var scoreText;
+                var cursors;
             }
 
             gameStart.prototype = {
                 create: function(){
                     score = 0;
-                    var addscoreButton = this.game.add.button(this.game.world.centerX, this.game.world.centerY-200, "addscoreImage", this.addScore, this);
-                    addscoreButton.anchor.setTo(0.5,0.5);
-                    var gameoverButton = this.game.add.button(this.game.world.centerX, this.game.world.centerY+200, "gameoverImage", this.endGame, this);
-                    gameoverButton.anchor.setTo(0.5,0.5);
-                    scoreText = this.game.add.text(this.game.world.centerX, this.game.world.centerY, "Score: "+score, { font: "50px Arial", fill: "#F0FFFF", align: "center" });
-                    scoreText.anchor.set(0.5);
+                    // var addscoreButton = this.game.add.button(this.game.world.centerX, this.game.world.centerY-200, "addscoreImage", this.addScore, this);
+                    // addscoreButton.anchor.setTo(0.5,0.5);
+                    // var gameoverButton = this.game.add.button(this.game.world.centerX, this.game.world.centerY+200, "gameoverImage", this.endGame, this);
+                    // gameoverButton.anchor.setTo(0.5,0.5);
+                    // scoreText = this.game.add.text(this.game.world.centerX, this.game.world.centerY, "Score: "+score, { font: "50px Arial", fill: "#F0FFFF", align: "center" });
+                    // scoreText.anchor.set(0.5);
+
+                    this.map = this.game.add.tilemap("map");
+                    this.map.addTilesetImage("simples_pimples", "tiles");
+                    this.backgroundlayer = this.map.createLayer("bgLayer");
+                    this.blockedLayer = this.map.createLayer("collideLayer");
+                    
+
+                    this.backgroundlayer.resizeWorld();
+                    cursors = this.game.input.keyboard.createCursorKeys();
                 },
                 addScore: function(){
                     score++;
@@ -19,5 +29,24 @@ var gameStart = function (game) {
                 },
                 endGame: function(){
                     this.game.state.start("GameOver", true, false, score);
+                },
+                update: function(){
+                    if (cursors.up.isDown)
+                    {
+                        this.game.camera.y -= 50;
+                    }
+                    else if (cursors.down.isDown)
+                    {
+                        this.game.camera.y += 50;
+                    }
+
+                    if (cursors.left.isDown)
+                    {
+                        this.game.camera.x -= 50;
+                    }
+                    else if (cursors.right.isDown)
+                    {
+                        this.game.camera.x += 50;
+                    }
                 }
             }
