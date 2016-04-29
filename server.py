e582e5f35febd64c90e823ecfcd229ac720ad60b
Ondrej Platek <ondrej.platek@seznam.cz>
2016-04-29 13:38:32 +0200

    fix static - work

--- server.py
+++ server.py
@@ -1,14 +1,10 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template
 app = Flask(__name__, static_url_path='')
+
 
 @app.route('/')
 def render_main_html():
     return render_template("index.html")
-
-@app.route('/js/<path:path>')
-def send_js(path):
-    return send_from_directory('js', path)
-
 
 
 if __name__ == '__main__':
