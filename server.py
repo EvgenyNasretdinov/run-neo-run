ed4bccb7c12edb89f68906ba7506aa788c96e586
Evgeny Nasretdinov <evgeny.nasretdinov@gmail.com>
2016-06-03 11:47:24 +0200

    simple logging, sending position to navigator from runner

--- server.py
+++ server.py
@@ -26,12 +26,11 @@ elif async_mode == 'gevent':
     from gevent import monkey
     monkey.patch_all()
 
-import time
-from threading import Thread
+import logging
+import datetime
 from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
-
 
 
 app = Flask(__name__, static_url_path='')
@@ -41,6 +40,9 @@ socketio = SocketIO(app, async_mode=async_mode)
 
 current_room = None
 room_count = 0
+LOGFILE_NAME = 'game_logs.log'
+app.logger.setLevel(logging.INFO)
+app.logger.addHandler(logging.FileHandler(LOGFILE_NAME))
 
 
 @app.route('/')
@@ -50,8 +52,13 @@ def render_main_html():
 @socketio.on('my event', namespace='/game')
 def test_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
+    app.logger.info('\t'.join([datetime.datetime.today().ctime(), str(message['room']), str(message['data'])]))
     emit('my response',
          {'data': message['data'], 'count': session['receive_count']}, room=message['room'])
+
+@socketio.on('send_position', namespace='/game')
+def position_message(message):
+    emit('get_position', {'position': message['position']}, room=message['room'])
 
 
 @socketio.on('connect', namespace='/game')
@@ -59,13 +66,13 @@ def join():
     global current_room
     global room_count
     if current_room:
-        emit('set room', {'room': current_room})
+        emit('set room', {'room': current_room, 'runner': True})
         join_room(current_room)
         current_room = None
     else:
         room_count += 1
         current_room = room_count
-        emit('set room',{'room': current_room})
+        emit('set room', {'room': current_room, 'runner': False})
         join_room(current_room)
 
 
