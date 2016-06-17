4e4504a87535975629d197a7bf8c525b60793b84
Evgeny Nasretdinov <evgeny.nasretdinov@gmail.com>
2016-06-17 02:00:59 +0200

    collision with walls, try to send position

--- static/js/gamepreload.js
+++ static/js/gamepreload.js
@@ -1,17 +1,25 @@
-var gamePreload = function(game) {}
+var gamePreload = function(game) {
+    var socket;
+    var currentRoom;
+}
 
             gamePreload.prototype = {
+                init: function(socket, currentRoom){
+                    this.socket = socket;
+                    this.currentRoom = currentRoom;
+                },
                 preload: function(){
                     this.game.load.image("gameoverImage", "images/gameover-button.png");
                     this.game.load.image("addscoreImage", "images/addscore-button.png");
                     this.game.load.tilemap("map", "tilemaps/test.json", null, Phaser.Tilemap.TILED_JSON);
                     this.game.load.image("tiles", "images/tiles.png");
+                    this.game.load.image("arrow", "images/arrow.png");
                 },
                 create: function(){
                     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
                     this.scale.pageAlignHorizontally = true;
                     this.scale.pageAlignVertically = true;
                     this.game.physics.startSystem(Phaser.Physics.ARCADE);
-                    this.game.state.start("GameStart");
+                    this.game.state.start("GameStart", true, false, this.socket, this.currentRoom);
                 }
             }
--- static/js/gamestart.js
+++ static/js/gamestart.js
@@ -2,9 +2,18 @@ var gameStart = function (game) {
                 score = 0;
                 var scoreText;
                 var cursors;
+                var player;
+                var positionXTest = 0;
+                var pXTold = positionXTest;
+                var socket;
+                var currentRoom;
             }
 
             gameStart.prototype = {
+                init: function(socket, currentRoom){
+                    this.socket = socket;
+                    this.currentRoom = currentRoom;
+                },
                 create: function(){
                     score = 0;
                     // var addscoreButton = this.game.add.button(this.game.world.centerX, this.game.world.centerY-200, "addscoreImage", this.addScore, this);
@@ -18,9 +27,26 @@ var gameStart = function (game) {
                     this.map.addTilesetImage("simples_pimples", "tiles");
                     this.backgroundlayer = this.map.createLayer("bgLayer");
                     this.blockedLayer = this.map.createLayer("collideLayer");
-                    
 
+
+                    this.map.setCollisionBetween(1, 2000, true, "collideLayer");
                     this.backgroundlayer.resizeWorld();
+
+
+                    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+                    
+                    this.player = this.game.add.sprite(80, 80, "arrow");
+                    this.player.anchor.set(0.5);
+
+                    this.game.physics.arcade.enable(this.player);
+
+                    // player.body.setSize(32, 32, 0, 0);
+                    // player.body.maxAngular = 500;
+                    // player.body.angularDrag = 50;
+
+                    this.game.camera.follow(this.player);
+
                     cursors = this.game.input.keyboard.createCursorKeys();
                 },
                 addScore: function(){
@@ -31,22 +57,44 @@ var gameStart = function (game) {
                     this.game.state.start("GameOver", true, false, score);
                 },
                 update: function(){
+
+                    this.socket.emit('send_position',{position: this.player.body.x, room: currentRoom});
+
+                    this.player.body.velocity.x = 0;
+                    this.player.body.velocity.y = 0;
+                    // player.body.angularVelocity = 0;
+
+                   if (this.pXTold != this.positionXTest){
+                        console.log(this.positionXTest);
+                        this.pXTold = this.positionXTest;
+                   } 
+
                     if (cursors.up.isDown)
                     {
-                        this.game.camera.y -= 50;
+                        this.player.body.velocity.y -= 350;
+                        // this.game.camera.y -= 50;
                     }
                     else if (cursors.down.isDown)
                     {
-                        this.game.camera.y += 50;
+                        this.player.body.velocity.y += 350;
+                        // this.game.camera.y += 50;
                     }
 
                     if (cursors.left.isDown)
                     {
-                        this.game.camera.x -= 50;
+                        this.player.body.velocity.x -= 350;
+                        // this.game.camera.x -= 50;
                     }
                     else if (cursors.right.isDown)
                     {
-                        this.game.camera.x += 50;
+                        this.player.body.velocity.x += 350;
+                        // this.game.camera.x += 50;
                     }
+
+                    this.game.physics.arcade.collide(this.player, this.blockedLayer);
+                },
+                render: function(){
+                        this.game.debug.body(this.player);
                 }
+
             }
